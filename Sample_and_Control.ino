6d19489abd4fe49e5aca07b33a29c60edbe8a7d1
Gavin <gavinjohnson@outlook.com>
2015-04-18 13:08:11 -0600

    Update

--- Sample_and_Control.ino
+++ Sample_and_Control.ino
@@ -3,6 +3,12 @@ This program is to be used to sample the arduino adcs and return data to a
 host device when requested using the character 'S', sent from the host.
 Additionally, it allows for control of an led from the host pc by the
 characters 'O' and 'X'.
+
+COMMAND SUMMARY:
+'S': Send data vector
+'O': LED off
+'X': LED on
+
 **/
 
 
@@ -65,7 +71,17 @@ void loop()
 
 // adc read interrupt at 120Hz
 ISR(TIMER1_COMPA_vect){
+	// Note 2 byte data structure as follows:
+	// [4 bits: channel number][2 bits: unused][10 bits: adc read data]
 	adc[0][idx] = analogRead(A0) | 0x0000; // read adc channel 0
+/**	adc[1][idx] = analogRead(A1) | 0x1000; // read adc channel 1
+	adc[2][idx] = analogRead(A2) | 0x2000; // read adc channel 2
+	adc[3][idx] = analogRead(A3) | 0x3000; // read adc channel 3
+	adc[4][idx] = analogRead(A4) | 0x4000; // read adc channel 4
+	adc[5][idx] = analogRead(A5) | 0x5000; // read adc channel 5
+	adc[6][idx] = analogRead(A6) | 0x6000; // read adc channel 6
+	adc[7][idx] = analogRead(A7) | 0x7000; // read adc channel 7**/
+
 	idx++;
 	if (idx == vecSize)
 	{
@@ -90,8 +106,22 @@ void sendVec()
 			idx_send = idx_send - vecSize;
 		}
 		// write the adc data
-		Serial.write(highByte(adc[0][idx_send]));
+		Serial.write(highByte(adc[0][idx_send])); // Send channel 0
 		Serial.write(lowByte(adc[0][idx_send]));
+/**		Serial.write(highByte(adc[1][idx_send])); // Send channel 1
+		Serial.write(lowByte(adc[1][idx_send]));
+		Serial.write(highByte(adc[2][idx_send])); // Send channel 2
+		Serial.write(lowByte(adc[2][idx_send]));
+		Serial.write(highByte(adc[3][idx_send])); // Send channel 3
+		Serial.write(lowByte(adc[3][idx_send]));
+		Serial.write(highByte(adc[4][idx_send])); // Send channel 4
+		Serial.write(lowByte(adc[4][idx_send]));
+		Serial.write(highByte(adc[5][idx_send])); // Send channel 5
+		Serial.write(lowByte(adc[5][idx_send]));
+		Serial.write(highByte(adc[6][idx_send])); // Send channel 6
+		Serial.write(lowByte(adc[6][idx_send]));
+		Serial.write(highByte(adc[7][idx_send])); // Send channel 7
+		Serial.write(lowByte(adc[7][idx_send]));**/
 		idx_send++;
 		cnt++;
 	}
